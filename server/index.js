21f0529a23a7616951790165db84e046452055c9
egilson.cabral <egilson.cabral@dataprev.gov.br>
2019-04-18 09:26:28 -0300

    Removendo codigos comentados

--- server/index.js
+++ server/index.js
@@ -8,9 +8,3 @@ app.use(cors());
 app.use(proxy("http://10.100.6.76:5000"))
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
-
-
- // REGRAS DE PROXY PARA AUTENTICAÇÃO DE USUÁRIO E ACESSO ÀS FEX
- // app.get('/api/:p(*)',  proxy('http://10.100.6.76:5000'));
- // app.get('/api/*',  proxy('http://10.100.6.76:5000'));
- // app.get('/api/:p(*)',  proxy('http://10.100.6.76:5000'));
