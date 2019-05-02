f5d35bfc456b2ca5c5db4f3d40dd96a9739ed6bd
fcarlos.silva <fcarlos.silva@dataprev.gov.br>
2019-05-02 15:50:29 -0300

    Implementando o componente de login.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
 import Home from './components/Home';
+import Login from './components/Login';
+import Content from './components/Content';
+import PrivateRoute from './components/PrivateRoute';
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Home/>
-      </div>
+      <Router>
+        <div className="App">
+          <Route exact path="/" component={Login} />
+          <PrivateRoute exact path ="/home" component={Home} />
+          <PrivateRoute exact path ="/pagina" component={Content} />
+        </div>
+      </Router>
     );
   }
 }
