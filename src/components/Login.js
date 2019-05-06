b5193fa3630e33dddc023aa23da379cbc0fe4863
fcarlos.silva <fcarlos.silva@dataprev.gov.br>
2019-05-06 15:41:35 -0300

    Implementando loading e mensagem de erro na tela de login.

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,6 @@
 import React from 'react';
-// import {Alert, Spinner} from 'react-bootstrap'
-import {Button} from 'react-bootstrap'
+import {Alert, Button} from 'react-bootstrap';
+import CustomSpinner from "../components/CustomSpinner";
 
 
 import * as FexAPI from '../utils/FexAPI';
@@ -13,7 +13,10 @@ class Login extends React.Component {
     super(props);
     this.state = {
       usuario: "",
-      senha: ""
+      senha: "",
+      msgErroAuth: "",
+      autenticando: false,
+      showErro: false,
     }
   }
 
@@ -24,17 +27,30 @@ class Login extends React.Component {
     })
   }
 
+  iniciarAutenticacao = autenticando => {
+    this.setState({autenticando})
+  }
+
+  showErroAuth = (show, msg) => {
+    this.setState({
+      showErro: show,
+      msgErroAuth: msg
+    });
+    this.iniciarAutenticacao(false);
+  }
+
   handleSubmit = event => {
     event.preventDefault();
+    this.iniciarAutenticacao(true);
     FexAPI.login(this.state.usuario, this.state.senha, (retornoLogin) => {
       let result = retornoLogin.querySelector('result');
       if (result.getAttribute('value') > 0) {
         this.keepTokenSession(result.getAttribute('CSRFTokenValue'));
         this.redirectUser("/home");
       } else if (result.getAttribute('value') === '-1') {
-        alert('Usuário não tem permissão para acessar a aplicação');
+        this.showErroAuth(true, 'Usuário não tem permissão para acessar a aplicação')
       } else{
-        alert('Usuário ou Senha inválidos');
+        this.showErroAuth(true, 'Usuário ou Senha inválidos')
       }
     })
   }
@@ -49,34 +65,44 @@ class Login extends React.Component {
 
   render() {
     return (
-      <div className="form-container">
-        <div className="login-form" onSubmit={this.handleSubmit}>
-          <form>
-            <div className="form-header">
-              <span className="app-name">Gerador Painel Estratégico</span>
-              <span className="app-desc">Controle de Acesso</span>
-            </div>
-            <div className="form-group">
-                <label htmlFor="inputNome">Usuário</label>
-                <input type="text" name="usuario"
-                       placeholder="Usuário"
-                       value={this.state.usuario}
-                       className="form-control" id="inputUsuario"
-                       onChange={this.handleInputChange} required/>
-            </div>
-            <div className="form-group">
-                <label htmlFor="inputNome">Senha</label>
-                <input type="password" name="senha" value={this.state.senha}
-                       placeholder="Senha"
-                       className="form-control" id="inputSenha"
-                       onChange={this.handleInputChange} required/>
-            </div>
-            <div className="form-buttons">
-              <Button type="submit" variant="primary" value="Submit">Acessar</Button>
-            </div>
-          </form>
+      <React.Fragment>
+        <Alert dismissible show={this.state.showErro} variant="danger"
+          onClick={() => this.showErroAuth(false,'')}>
+          <p> {this.state.msgErroAuth} </p>
+        </Alert>
+
+        <div className="form-container">
+          <div className="login-form" onSubmit={this.handleSubmit}>
+            <form>
+              <div className="form-header">
+                <span className="app-name">Gerador Painel Estratégico</span>
+                <span className="app-desc">Controle de Acesso</span>
+              </div>
+              <div className="form-group">
+                  <label htmlFor="inputNome">Usuário</label>
+                  <input type="text" name="usuario"
+                         placeholder="Usuário"
+                         value={this.state.usuario}
+                         className="form-control" id="inputUsuario"
+                         onChange={this.handleInputChange} required/>
+              </div>
+              <div className="form-group">
+                  <label htmlFor="inputNome">Senha</label>
+                  <input type="password" name="senha" value={this.state.senha}
+                         placeholder="Senha"
+                         className="form-control" id="inputSenha"
+                         onChange={this.handleInputChange} required/>
+              </div>
+              <div className="form-buttons">
+                <Button type="submit" variant="primary" value="Submit">
+                  {this.state.autenticando ?
+                    <CustomSpinner msg="Autenticando..." /> : "Acessar"}
+                </Button>
+              </div>
+            </form>
+          </div>
         </div>
-      </div>
+      </React.Fragment>
     )
   }
 
